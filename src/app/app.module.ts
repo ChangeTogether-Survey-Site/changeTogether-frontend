5cb41187ceb4dd844dc8d95a473dd0c800031bbe
Alejandro A <ale@doges.dev>
2022-11-12 21:41:01 -0500

    added angular material theme indigo/pink to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SurveyCreateComponent } from './surveys/survey-create/survey-create.component';
-import { PostCreateComponent } from './posts/post-create/post-create.component'
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { PostCreateComponent } from './posts/post-create/post-create.component'
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
