ff7bdd1de7bde9202e7b2b27bfaa016b7ca08fc0
Alejandro A <ale@doges.dev>
2022-11-12 20:44:58 -0500

    created post-create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SurveyCreateComponent} from './surveys/survey-create/survey-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SurveyCreateComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/surveys/survey-create/survey-create.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-survey-create',
+  templateUrl: './survey-create.component.html'
+})
+export class SurveyCreateComponent {
+
+}
