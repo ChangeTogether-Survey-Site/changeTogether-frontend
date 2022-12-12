81013c5d1ce3652a74e361865799a9fac7866741
Alejandro A <ale@doges.dev>
2022-12-12 04:53:32 -0500

    added authentication timer, session storage with token and token expiration, auto auth method that logs in if a auth object is in local storage to persist login status

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -9,6 +9,7 @@ import { AuthData } from './auth-data.model';
 export class AuthService {
     private isAuthenticated = false;
     private token: string;
+    private tokenTimer: NodeJS.Timer;
     private authStatusListener = new Subject<boolean>();
 
     constructor(private http: HttpClient, private router: Router){}
@@ -35,19 +36,42 @@ export class AuthService {
 
     loginUser(email: string, password: string){
         const authData: AuthData = {email: email, password: password};
-        this.http.post<{token: string}>("http://localhost:3500/api/user/login", authData)
+        this.http.post<{token: string, expiresIn: number}>("http://localhost:3500/api/user/login", authData)
             .subscribe(response => {
                 console.log(response);
                 const token = response.token;
                 this.token = token;
                 // broadcast to other components that the auth status has changed
                 if (token) {
+                    const expiresInDuration = response.expiresIn;
+                    this.setAuthTimer(expiresInDuration);
                     this.isAuthenticated = true;
                     this.authStatusListener.next(true);
+                    // set expiration
+                    const now = new Date();
+                    const expirationDate = new Date(now.getTime() + expiresInDuration * 1000);
+                    console.log(expirationDate);
+                    this.saveAuthData(token, expirationDate)
+
                     this.router.navigate(["/"]);
                 }
-
         });
+    }
+
+    autoAuthUser(){
+        const authInfo = this.getAuthData();
+        if (!authInfo){
+            return;
+        }
+        const now = new Date();
+        const expiresIn = authInfo.expirationDate.getTime() - now.getTime();
+        if (expiresIn > 0){
+            this.token = authInfo.token;
+            this.isAuthenticated = true;
+            this.setAuthTimer(expiresIn / 1000);
+            this.authStatusListener.next(true);
+        }
+
     }
 
     logout() {
@@ -55,5 +79,37 @@ export class AuthService {
         this.isAuthenticated = false;
         this.authStatusListener.next(false);
         this.router.navigate(["/"]);
+        clearTimeout(this.tokenTimer);
+        this.clearAuthData();
+    }
+
+    private saveAuthData(token: string, expirationDate: Date){
+        localStorage.setItem('token', token);
+        localStorage.setItem('expiration', expirationDate.toISOString());
+
+    }
+
+    private clearAuthData(){
+        localStorage.removeItem('token');
+        localStorage.removeItem('expiration');
+    }
+
+    private getAuthData(){
+        const token = localStorage.getItem('token');
+        const expirationDate = localStorage.getItem('expiration');
+        if (!token || !expirationDate){
+            return;
+        }
+        return {
+            token: token,
+            expirationDate: new Date(expirationDate)
+        }
+    }
+
+    private setAuthTimer(duration: number){
+        console.log("Setting timer: " + duration);
+        this.tokenTimer = setTimeout(() => {
+            this.logout();
+        }, duration * 1000);
     }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -16,6 +16,7 @@ export class HeaderComponent implements OnInit{
   constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.userIsAuthenticated = this.authService.getIsAuthenticated();
     this.authListenerSubs = this.authService.getAuthStatusListener().subscribe( isAuthenticated => {
       this.userIsAuthenticated = isAuthenticated;
     });
